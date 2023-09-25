d17d96716db529c1ec88b5ff287b3e6f5c0b28f9
mjpark <mjpark@daou.co.kr>
2023-09-26 02:43:40 +0900

    feat: 카프카 프로듀서 key에 따른 파티션 지정

---
+++ Chapter3/3.4.1 kafka-producer/simple-kafka-producer/src/main/java/com/example/customPartitioner/CustomPartitioner.java
@@ -0,0 +1,36 @@
+package com.example.customPartitioner;
+
+import java.util.List;
+import java.util.Map;
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.InvalidRecordException;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.utils.Utils;
+
+public class CustomPartitioner  implements Partitioner {
+
+    @Override
+    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes,
+                         Cluster cluster) {
+
+        if (keyBytes == null) {
+            throw new InvalidRecordException("Need message key");
+        }
+        if (key.equals("Pangyo"))
+            // 무조건 0번 파티션
+            return 0;
+
+        // 나머지는 hash 값으로 사용한다.
+        List<PartitionInfo> partitions = cluster.partitionsForTopic(topic);
+        int numPartitions = partitions.size();
+        return Utils.toPositive(Utils.murmur2(keyBytes)) % numPartitions;
+    }
+
+
+    @Override
+    public void configure(Map<String, ?> configs) {}
+
+    @Override
+    public void close() {}
+}
