fe2c92ee07b57f39a8506e1c7a148fae043ce6e8
mjpark <mjpark@daou.co.kr>
2023-09-26 02:41:55 +0900

    feat: 카프카 프로듀서 key-value

---
+++ Chapter3/3.4.1 kafka-producer/simple-kafka-producer/src/main/java/com/example/ProducerWithKeyValue.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class ProducerWithKeyValue {
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
+
+    public static void main(String[] args) {
+
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+
+        // 토픽명, 메시지 키, 메시지 값 전달
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "Pangyo", "Pangyo");
+        producer.send(record);
+        ProducerRecord<String, String> record2 = new ProducerRecord<>(TOPIC_NAME, "Busan", "Busan");
+        producer.send(record2);
+        producer.flush();
+        producer.close();
+    }
+}
