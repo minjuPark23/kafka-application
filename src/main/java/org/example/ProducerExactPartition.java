cc95c701857386f289492ffe54b5d7c45e34edda
mjpark <mjpark@daou.co.kr>
2023-09-26 13:15:06 +0900

    feat: 카프카 프로듀서 특정 파티션 지정,

---
+++ src/main/java/org/example/ProducerExactPartition.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class ProducerExactPartition {
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
+
+    public static void main(String[] args) {
+
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+
+        int partitionNo = 0;
+        // 토픽명, 파티션 번호, 키, 값
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, partitionNo, "Pangyo", "Pangyo");
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
+    }
+}
