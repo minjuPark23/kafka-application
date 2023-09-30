3a090118d8c167b319a296c18eeb63f909e341a8
minjuPark23 <minj5541@naver.com>
2023-09-30 20:31:16 +0900

    feat: 카프카 컨슈머 생성

---
+++ src/main/java/org/example/consumer/SimpleConsumer.java
@@ -0,0 +1,45 @@
+package org.example.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class SimpleConsumer {
+    private final static Logger logger = LoggerFactory.getLogger(SimpleConsumer.class);
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
+    private final static String GROUP_ID = "test-group";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        // 특정 토픽을 구독하기 위해 groupId
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        // producer에서 직렬화되어 토픽에 저장되기 때문에
+        // 추가적으로 역직렬화에 대한 설정이 필요하다.
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
+
+        // 1개 이상의 토픽을 구독하는 consumer Instance
+        consumer.subscribe(Arrays.asList(TOPIC_NAME));
+
+        // 데이터 가져오기
+        while (true) {
+            // poll: 병렬 처리된 records 가져오기
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("record:{}", record);
+            }
+        }
+    }
+}
